3f8dc1299ff0c8782f236fabd5734abfb5979a32
rik0 <rik0@87ce5da4-9c21-0410-9cd6-9bdbc124f340>
2007-01-03 09:58:44 +0000

    Updated versions.

--- trunk/LogNotifier/setup.py
+++ trunk/LogNotifier/setup.py
@@ -59,7 +59,7 @@ infos = {
 'license'          : 'BSD license',
 'name'             : 'LogNotifier',
 'platforms'        : 'MacOS X',
-'version'          : '0.3.2',
+'version'          : '0.3.3',
 }
 
 py2app_options = {
